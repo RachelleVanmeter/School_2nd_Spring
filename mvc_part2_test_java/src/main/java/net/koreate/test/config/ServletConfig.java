d27aa8d29a9f9244842532f0fbde0535851d8379
KimTaeMin99 <ktm06069@naver.com>
2019-06-11 10:47:15 +0900

    스프링의 설정 정보를 자바로 바꿔 보기

---
+++ mvc_part2_test_java/src/main/java/net/koreate/test/config/ServletConfig.java
@@ -0,0 +1,31 @@
+package net.koreate.test.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@ComponentScan(basePackages = {
+		"net.koreate.test.controller"
+})
+public class ServletConfig implements WebMvcConfigurer {
+	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INT/views/");
+		viewResolver.setSuffix(".jsp");
+		registry.viewResolver(viewResolver);
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+	
+}
---
+++ mvc_part2_test_java/src/main/webapp/member.sql
@@ -0,0 +1,4 @@
+SELECT * FROM tbl_member;
+
+DELETE FROM tbl_member;
+
