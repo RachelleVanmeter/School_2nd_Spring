feef61450c17686997ae3e361425f851a3d2c733
KimTaeMin99 <ktm06069@naver.com>
2019-06-21 12:48:53 +0900

    인터셉터 공부중

---
+++ mvc_part7_interceptor/src/main/java/net/koreate/interceptor/TestInterceptor.java
@@ -0,0 +1,50 @@
+package net.koreate.interceptor;
+
+import java.lang.reflect.Method;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class TestInterceptor extends HandlerInterceptorAdapter{
+
+	// Controller 요청 처리전
+	@Override
+	public boolean preHandle(
+			HttpServletRequest request, 
+			HttpServletResponse response, 
+			Object handler)
+			throws Exception {
+		System.out.println("preHandle START");
+		HandlerMethod method =(HandlerMethod)handler;
+		Method methodObj = method.getMethod();
+		System.out.println("Bean : " + method.getBean());
+		System.out.println("method : " + methodObj);
+		System.out.println("preHandle END");
+		return true;
+	}
+
+	// Controller 요청 처리 후
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		System.out.println("postHandle START");
+		
+		System.out.println("postHandle END");
+	}
+
+	// DispatcherServlet 화면 전송 완료 후 
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		System.out.println("afterCompletion start");
+		System.out.println("afterCompletion end");
+	}
+	
+	
+	
+
+}
