7f614e2009ed904070aa4e8489f1966e6aa97893
KimTaeMin99 <ktm06069@naver.com>
2019-06-24 13:46:12 +0900

    clean

--- mvc_part8_board/src/main/java/net/koreate/aop/AOPAdvice.java
+++ mvc_part8_board/src/main/java/net/koreate/aop/AOPAdvice.java
@@ -10,21 +10,21 @@ import org.springframework.stereotype.Controller;
 @Controller
 @Aspect
 public class AOPAdvice {
-	
+
 	@Around("execution(void net.koreate.service.UserService*.*(..))")
-	public void checkUserService(ProceedingJoinPoint pjp) throws Throwable{
+	public void checkUserService(ProceedingJoinPoint pjp) throws Throwable {
 		System.out.println("AOP checkUserService START");
-		
+
 		System.out.println(Arrays.toString(pjp.getArgs()));
 		pjp.proceed();
-		
+
 		System.out.println("AOP checkUserService END");
 	}
-	
+
 	@Around("execution(!void net.koreate.service.UserService*.*(..))")
-	public Object checkUserServiceObject(ProceedingJoinPoint pjp) throws Throwable{
+	public Object checkUserServiceObject(ProceedingJoinPoint pjp) throws Throwable {
 		System.out.println("AOP checkUserServiceObject START");
-		
+
 		System.out.println(Arrays.toString(pjp.getArgs()));
 		Object o = pjp.proceed();
 		System.out.println(o);
--- mvc_part8_board/src/main/java/net/koreate/controller/UserController.java
+++ mvc_part8_board/src/main/java/net/koreate/controller/UserController.java
@@ -16,32 +16,33 @@ import net.koreate.vo.UserVO;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-	
+
 	@Inject
-	UserService us;	
-	
+	UserService us;
+
 	@GetMapping("/signIn")
-	public String signIn() {return "/user/signIn";}
-	
+	public String signIn() {
+		return "/user/signIn";
+	}
+
 	@RequestMapping("/signUp")
-	public String signUp() {return "/user/signUp";}
+	public String signUp() {
+		return "/user/signUp";
+	}
 
 	@PostMapping("/signUpPost")
-	public String signUpPost(UserVO vo, RedirectAttributes rttr) throws Exception{
+	public String signUpPost(UserVO vo, RedirectAttributes rttr) throws Exception {
 		System.out.println("signPost UserVO" + vo);
 		us.signUp(vo);
-		rttr.addFlashAttribute("message","회원가입성공");
+		rttr.addFlashAttribute("message", "회원가입성공");
 		return "redirect:/user/signIn";
 	}
-	
+
 	@PostMapping("/signInPost")
 	public ModelAndView signInPost(LoginDTO dto, ModelAndView mav) {
-		mav.addObject("loginDTO",dto);
+		mav.addObject("loginDTO", dto);
 		mav.setViewName("redirect:/");
 		return mav;
 	}
-	
+
 }
-
-
-
