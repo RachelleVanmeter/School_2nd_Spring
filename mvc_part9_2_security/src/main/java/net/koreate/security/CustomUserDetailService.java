e0429dc7532f29912e038b30292c329876f5a1a3
KimTaeMin99 <ktm06069@naver.com>
2019-07-03 11:44:48 +0900

    기본 시큐리티 완료

--- mvc_part9_2_security/src/main/java/net/koreate/security/CustomUserDetailService.java
+++ mvc_part9_2_security/src/main/java/net/koreate/security/CustomUserDetailService.java
@@ -1,15 +1,31 @@
 package net.koreate.security;
 
+import javax.inject.Inject;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import net.koreate.dao.MemberDao;
+import net.koreate.security.user.CustomUser;
+import net.koreate.vo.MemberVo;
+
 public class CustomUserDetailService implements UserDetailsService {
 
+	@Inject
+	MemberDao dao;
+	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		
-		return null;
+		MemberVo member = null;
+		System.out.println("loadUserByUsername");
+		try {
+			member = dao.read(username);
+			System.out.println("quired by Member : " + member);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return member == null ? null : new CustomUser(member);
 	}
 
 }
--- mvc_part9_2_security/src/main/webapp/WEB-INF/views/test/master.jsp
+++ mvc_part9_2_security/src/main/webapp/WEB-INF/views/test/master.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,5 +9,18 @@
 </head>
 <body>
 	<h1>MASTER</h1>
+	<sec:authorize access="isAuthenticated()">
+		<p>principal : <sec:authentication property="principal"/></p>
+		<p>ID : <sec:authentication property="principal.username"/></p>
+		<p>MemberVo : <sec:authentication property="principal.member"/></p>
+		<p>MemberVo uname : <sec:authentication property="principal.member.uname"/></p>
+		<p>MemberVo authList : <sec:authentication property="principal.member.authList"/></p>
+		<sec:authentication property="principal.member" var="mem"/>
+		${mem.uid} <br>
+		${mem.upw} <br>
+		${mem.uname} <br>
+		${mem.regdate} <br>
+		${mem.updatedate} <br>
+	</sec:authorize>
 </body>
 </html>
